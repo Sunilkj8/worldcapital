befc5333aa07dbc8d91067a1b5c7664573f68f18
Sunilkj8 <109097652+Sunilkj8@users.noreply.github.com>
2024-09-15 12:07:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -265,6 +265,7 @@ let totalCorrect = 0;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
 let currentQuestion = {};
 
